7aa3425a36e1e61e22692f34f9e410c0f6396719
tan <cupitrh@gmail.com>
2026-04-10 16:35:00 +0800

    feat: add settings page

---
+++ pages/settings/index.js
@@ -0,0 +1,37 @@
+const Api = require('../../utils/api.js');
+
+Page({
+  onLoad() {
+    if (!getApp().isLoggedIn()) {
+      wx.navigateTo({ url: '/pages/login/index' });
+    }
+  },
+
+  goProfile() {
+    wx.showToast({ title: '功能开发中', icon: 'none' });
+  },
+
+  goHelp() {
+    wx.showToast({ title: '功能开发中', icon: 'none' });
+  },
+
+  goAbout() {
+    wx.showToast({ title: '功能开发中', icon: 'none' });
+  },
+
+  handleLogout() {
+    wx.showModal({
+      title: '确认退出',
+      content: '确定要退出登录吗？',
+      success: (res) => {
+        if (res.confirm) {
+          Api.logout();
+          wx.showToast({ title: '已退出', icon: 'success' });
+          setTimeout(() => {
+            wx.switchTab({ url: '/pages/mine/index' });
+          }, 1000);
+        }
+      }
+    });
+  }
+});
