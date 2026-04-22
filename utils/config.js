92034f6bffd6a1665727829abf96f5894c5b38c8
tan <cupitrh@gmail.com>
2026-04-22 10:05:36 +0800

    fix: update API base to test environment

--- utils/config.js
+++ utils/config.js
@@ -2,8 +2,8 @@
 // API 地址通过此文件配置，部署时修改此文件即可切换环境
 module.exports = {
   // API Base URL - 修改此处切换测试/生产环境
-  apiBase: 'https://miao.clawos.cc/api/v1',
+  apiBase: "https://miao-test.clawos.cc/api/v1",
 
   // 客服热线
-  customerServicePhone: '400-xxx-xxxx',
+  customerServicePhone: "400-xxx-xxxx",
 };
